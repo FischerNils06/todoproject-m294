eb70cc2e9225286a26d13dd83310cb14c170cd20
Nils Fischer <fischern@bzz.ch>
2022-10-06 12:48:38 +0200

    addtask implementiert

--- src/api.js
+++ src/api.js
@@ -1,10 +1,68 @@
+function createCell (text){
+    const cell = document.createElement('td')
+    cell.innerText = text;
+    return cell;
+}
+
+
+const deleteButton = document.createElement('button')
+deleteButton.innerText = "Delete"
+
+function rendertask(tasks) {
+
+const tablebody = document.querySelector('tbody');
+    tasks.forEach((task) => {
+        const tableRow = document.createElement('tr');
+        tableRow.appendChild(createCell(task.id));
+        tableRow.appendChild(createCell(task.title));
+        tableRow.appendChild(createCell(task.completed));
+        tableRow.appendChild(deleteButton);
+        tablebody.appendChild(tableRow);
+    });
+}
+
+
+    let tasksData;
+function indextasks() {
+    fetch('http://Localhost:3000/tasks')
+    .then((response) => 
+    response.json()
+    )
+    .then((data) => {
+        rendertask(data);
+    })
+
+
+}
+
+function createTask(task) {
+    
+        fetch('http://Localhost:3000/tasks', {
+        method: 'Post',
+        headers: {
+            'Content-Type' : 'application/json'
+        } ,
+        body: JSON.stringify({
+            title : tasktitle
+        })
+        
+    });
+    
+    
+    
+    
+};
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
-fetch('http://localhost:3000/#tasksGet')
-.then((response) => response.json())
-.then((response) => {
+indextasks();
+    document.getElementById('addButton').addEventListener('click', () => {
     
-    return response.json()
+        tasktitle = document.getElementById('tasktext').value;
+    
+        createTask(tasktitle);
+
+    });
 });
 
-});
